4018174af0edef2f96fdb776b69d2ee7d3f647f9
moemenbelleh <80589439+moemenbelleh@users.noreply.github.com>
2023-07-06 10:16:01 +0200

    first commit

---
+++ src/test/java/testpackage/welcomeTest.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class welcomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to test project1 for git");
+	}
+
+}
